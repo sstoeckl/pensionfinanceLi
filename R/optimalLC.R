e817286f00e8d7706e641e66c7fadbe7061b70c0
Diffform <sstoeckl@gmx.de>
2020-05-18 15:52:16 +0200

    use old optim function due to the possibility of setting tolerance levels for Nelder-mead

--- R/optimalLC.R
+++ R/optimalLC.R
@@ -15,28 +15,31 @@
 #' .load_parameters()
 #' initial_values <- c(0.6, 0.12, 0.5, 0.2, 0.96, 0.25, 0.25, 0.25)
 #' optimalLC(initial_values,upper_bounds=NULL,lower_bounds=NULL,ret_age=ret_age,ra=ra,delta=delta,beta=beta,c_age=c_age,gender=gender,gender_mortalityTable=gender_mortalityTable,
-#'          w0=w0,CF=CF,li=li,lg=lg,c1=c1,s1=s1,s2=s2,s3=s3,w2=w2,rho2=rho2,rho3=rho3,ret=ret[,,1:10],retr=retr[,,1:10],psi=psi,trace=1)
+#'          w0=w0,CF=CF,li=li,lg=lg,c1=c1,s1=s1,s2=s2,s3=s3,w2=w2,rho2=rho2,rho3=rho3,ret=ret[,,1:10],retr=retr[,,1:10],psi=psi,trace=1,reltol=1e-5)
 #'
 #' }
 #' @importFrom optimx optimx
 #'
 #' @export
 optimalLC <- function(initial_values,upper_bounds,lower_bounds,ret_age,ra,delta,beta,c_age,gender,gender_mortalityTable,w0,
-                      CF,li,lg,c1,s1,s2,s3,w2,rho2,rho3,ret,retr,psi,verbose=FALSE,warnings=FALSE,trace){
+                      CF,li,lg,c1,s1,s2,s3,w2,rho2,rho3,ret,retr,psi,verbose=FALSE,warnings=FALSE,trace=0,reltol=sqrt(.Machine$double.eps)){
   #mc <- data.frame(method=c("Nelder-Mead","L-BFGS-B"), maxit=c(50,50), maxfeval= c(50,50))
-  mc <- data.frame(method=c("Nelder-Mead"), maxit=c(50*9^2), maxfeval= c(50*9^2))
-  ivmat <- as.matrix(rbind(c(0.5,0.1,0,0,1,rep(1/3,3),65),
-                           c(0.5,0.1,0.1,0.1,1,rep(1/3,3),65)))
+  mc <- data.frame(method=c("Nelder-Mead"), maxit=c(50*9^2), maxfeval= c(50*9^2), reltol=sqrt(.Machine$double.eps)*100)
+  ivmat <- rbind(initial_values,
+                 as.matrix(rbind(c(0.5,0.1,0,0,1,rep(1/3,3)),
+                           c(0.5,0.1,0.1,0.1,1,rep(1/3,3)))))
   res <- NULL
   for (i in c(1:3)){
-    resn <- optimx::polyopt(par=initial_values,
-                            fn=.util_optim, gr=NULL,hess=NULL,#gr=function(x) pracma::gradient(util_optim,x),
+    resn <- optim(par=ivmat[i,],
+                            fn=.util_optim, #gr=NULL,hess=NULL,#gr=function(x) pracma::gradient(util_optim,x),
                             #lower=lower_bounds,
                             #upper=upper_bounds,
-                            methcontrol=mc,
+                            #methcontrol=mc,
+                            method="Nelder-Mead",
                             control=list(#all.methods=TRUE,
-                              #save.failures=TRUE, maxit=50*5,
-                              trace=trace),#, factr = 1e-10),
+                              reltol=reltol,
+                              trace=trace,
+                              maxit=50*9^2),#, factr = 1e-10),
                             #itnmax=50*3^2,
                             ret_age=ret_age,
                             ra=ra,delta=delta,beta=beta,c_age=c_age,gender=gender,
@@ -44,10 +47,9 @@ optimalLC <- function(initial_values,upper_bounds,lower_bounds,ret_age,ra,delta,
                             w0=w0,CF=CF,li=li,lg=lg,c1=c1,s1=s1,s2=s2,s3=s3,w2=w2,
                             rho2=rho2,rho3=rho3,
                             ret=ret,retr=retr,psi=psi,verbose=verbose,warnings=warnings)
-    res <- cbind("i"=1,"method"=c("Nelder-Mead"),data.frame(resn, row.names=NULL))
+    res <- tibble("i"=1,"method"=c("Nelder-Mead"),bind_rows(unlist(resn)))
     cat("Round ",i,"\n")
     if (min(res["convergence"])==0) {break()}
-    initial_values <- ivmat[i,]
   }
   return(res)
 }
--- R/optimalwc.R
+++ R/optimalwc.R
@@ -16,26 +16,27 @@
 #' initial_values <- c(0.5, 0.95, 0.25, 0.25, 0.25)
 #' outwc <- optimalwc(initial_values,upper_bounds=NULL,lower_bounds=NULL,ret_age=ret_age,c2=c2,nu2=nu2,nu3=nu3,
 #'          ra=18,delta=delta,beta=bbeta,c_age=c_age,gender=gender,gender_mortalityTable=gender_mortalityTable,
-#'          w0=w0,CF=CF,li=li,lg=lg,c1=c1,s1=s1,s2=s2,s3=s3,w2=w2,rho2=rho2,rho3=rho3,ret=ret[,,1:10],retr=retr[,,1:10],psi=psi,trace=1)
+#'          w0=w0,CF=CF,li=li,lg=lg,c1=c1,s1=s1,s2=s2,s3=s3,w2=w2,rho2=rho2,rho3=rho3,ret=ret[,,1:10],retr=retr[,,1:10],psi=psi,trace=1,reltol=1e-4)
 #'
 #' @importFrom optimx optimx
 #'
 #' @export
 optimalwc <- function(initial_values,upper_bounds,lower_bounds,ret_age,c2,nu2,nu3,ra,delta,beta,c_age,gender,gender_mortalityTable,w0,
-                      CF,li,lg,c1,s1,s2,s3,w2,rho2,rho3,ret,retr,psi,verbose=FALSE,warnings=FALSE,trace=0){
+                      CF,li,lg,c1,s1,s2,s3,w2,rho2,rho3,ret,retr,psi,verbose=FALSE,warnings=FALSE,trace=0,reltol=sqrt(.Machine$double.eps)){
   #mc <- data.frame(method=c("Nelder-Mead","L-BFGS-B"), maxit=c(50,50), maxfeval= c(50,50))
   mc <- data.frame(method=c("Nelder-Mead"), maxit=c(50*5^2), maxfeval= c(50*5^2))
-  ivmat <- as.matrix(rbind(c(0.5,1,rep(0,3)),c(1,1,rep(0,3))))
+  ivmat <- rbind(initial_values,as.matrix(rbind(c(0.5,1,rep(0,3)),c(1,1,rep(0,3)))))
   res <- NULL
   for (i in c(1:3)){
-    resn <- optimx::polyopt(par=initial_values,
-                fn=.util_optim_wc, gr=NULL,hess=NULL,#gr=function(x) pracma::gradient(util_optim,x),
+    resn <- optim(par=ivmat[i,],
+                fn=.util_optim_wc, #gr=NULL,hess=NULL,#gr=function(x) pracma::gradient(util_optim,x),
                 #lower=lower_bounds,
                 #upper=upper_bounds,
-                methcontrol=mc,
+                method="Nelder-Mead",
                 control=list(#all.methods=TRUE,
-                #save.failures=TRUE, maxit=50*5,
-                trace=trace),#, factr = 1e-10),
+                  reltol=reltol,
+                  trace=trace,
+                  maxit=50*5^2),#, factr = 1e-10),
                 #itnmax=50*3^2,
                 ret_age=ret_age,c2=c2,nu2=nu2,nu3=nu3,
                 ra=ra,delta=delta,beta=beta,c_age=c_age,gender=gender,
@@ -43,10 +44,9 @@ optimalwc <- function(initial_values,upper_bounds,lower_bounds,ret_age,c2,nu2,nu
                 w0=w0,CF=CF,li=li,lg=lg,c1=c1,s1=s1,s2=s2,s3=s3,w2=w2,
                 rho2=rho2,rho3=rho3,
                 ret=ret,retr=retr,psi=psi,verbose=verbose,warnings=warnings)
-    res <- cbind("i"=1,"method"=c("Nelder-Mead"),data.frame(resn, row.names=NULL))
+    res <- tibble("i"=1,"method"=c("Nelder-Mead"),bind_rows(unlist(resn)))
     cat("Round ",i,"\n")
     if (min(res["convergence"])==0) {break()}
-    initial_values <- ivmat[i,]
   }
   return(res)
 }
